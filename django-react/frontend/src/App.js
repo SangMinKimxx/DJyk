65ad29d0ef0845a931d01c2884074cef347bb68b
sunbugbug <ygy0803@gmail.com>
2021-11-15 23:09:06 +0900

    login incompleted

--- django-react/frontend/src/App.js
+++ django-react/frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import {BrowserRouter as Route, Switch} from 'react-router-dom';
+import React, {Suspense} from 'react';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';	// 추가
+import LoginPage from './components/UserPage/LoginPage';	// 추가
+import SignupPage from './components/UserPage/SignupPage';	// 추가
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Suspense fallback={(<div>...</div>)}>
+      <NavBar />
+      <div className="App">
+          <Switch>
+            <Route exact path="/login" component={LoginPage}></Route>	// 추가
+            <Route exact path="/signup" component={SignupPage}></Route>	// 추가
+          </Switch>
+        </div>
+    </Suspense>
+    );
 }
 
 export default App;
